811f51815eb97a2b7b42a78d89743fc2ec98a274
gulnaramari <gulnaramari@yandex.ru>
2025-08-15 21:15:34 +0300

    тестирование

--- myproject/config/urls.py
+++ myproject/config/urls.py
@@ -5,12 +5,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('labbook.urls', namespace='labbook')),
     path('', include('users.urls', namespace='users')),
+
+
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
