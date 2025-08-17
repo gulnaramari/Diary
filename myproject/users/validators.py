1b293ee51f5e1cfb2a7a7a88b6cb989a7b1717a2
gulnaramari <gulnaramari@yandex.ru>
2025-08-17 16:17:49 +0300

    изменение структуры проекта

--- myproject/users/validators.py
+++
@@ -1,9 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def image_validate(image):
-    """Функция для проверки размера загружаемого изображения."""
-    max_size = 5 * 1024 * 1024
-
-    if image.size > max_size:
-        raise ValidationError(" Размер изображения должен быть меньше 5 МБ.")
