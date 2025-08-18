7346c202346c0d44a7a8c55153f75884f6b35006
gulnaramari <gulnaramari@yandex.ru>
2025-08-18 03:30:47 +0300

    попытка деплоя

--- config/settings.py
+++ config/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG").lower() == "true"
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 AUTH_USER_MODEL = "users.Employee"
 
